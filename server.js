8bb477dc222a0bbc0a581b9f985a2a10e6aaebc4
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-07 23:28:43 -0500

    forgot to add express node module

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./Develop/routes');
+//*TUTOR QUESTION: Since the model path locations are within the develop/routes path. Do I not need to import the models folder?
 const sequelize = require('./Develop/config/connection');
-// import sequelize connection
+// imported sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
