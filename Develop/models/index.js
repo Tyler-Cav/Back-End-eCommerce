aaaff804fccedd20f3405d577916a5cc3d9a7fde
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-09 23:37:11 -0500

    updated index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -18,9 +18,9 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {through: ProductTag})
 
 module.exports = {
   Product,
